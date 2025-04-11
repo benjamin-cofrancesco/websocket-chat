8474afbc5b9b338c849f3f543d3d6b495f6f99d0
Benjamin Cofrancesco <benjamin@whitefoxboutique.com>
2025-04-11 16:09:07 +1000

    feat: connected to websocket

--- server.js
+++ server.js
@@ -1,1 +1,48 @@
-hello;
+import { createServer } from "http";
+import crypto from "crypto";
+
+const PORT = 3333;
+const WEBSOCKET_KEY = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+const server = createServer((req, res) => {
+  res.writeHead(200);
+  res.end("yo");
+}).listen(PORT);
+
+const handleSocketUpgrade = (req, socket, head) => {
+  const { "sec-websocket-key": webClientSocketKey } = req.headers;
+
+  const headers = handleSocketHandshake(webClientSocketKey);
+
+  socket.write(headers);
+};
+
+const handleSocketHandshake = (id) => {
+  const acceptKey = generateSocketAccept(id);
+  const headers = [
+    "HTTP/1.1 101 Switching Protocols",
+    "Upgrade: websocket",
+    "Connection: Upgrade",
+    `Sec-WebSocket-Accept: ${acceptKey}`,
+    "",
+  ]
+    .map((line) => line.concat("\r\n"))
+    .join("");
+
+  return headers;
+};
+
+const generateSocketAccept = (id) => {
+  const sha1 = crypto.createHash("sha1");
+  sha1.update(id + WEBSOCKET_KEY);
+  return sha1.digest("base64");
+};
+
+server.on("upgrade", handleSocketUpgrade);
+const errors = ["uncaughtException", "unhandledRejection"];
+
+for (const error of errors) {
+  process.on(error, (err) => {
+    // TODO: handle errors
+  });
+}
