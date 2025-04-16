ee692b3dbc245a2b2334ff35e21609d3e43324e8
Benjamin Cofrancesco <benjamin@whitefoxboutique.com>
2025-04-16 12:35:12 +1000

    feat: added an event when connecting

--- chat-socket.js
+++ chat-socket.js
@@ -2,6 +2,17 @@ const socket = new WebSocket("ws://localhost:3333");
 
 window.ChatSocket = socket;
 socket.onopen = (event) => console.log("WebSocket is connected!");
-socket.onmessage = (msg) => console.log(msg);
+socket.onmessage = (msg) => {
+  console.log(msg);
+  const messageEvent = JSON.parse(msg.data);
+
+  const { user, message, type } = messageEvent.data;
+
+  if (!type) return;
+
+  const event = new CustomEvent(type, { detail: { user, message } });
+
+  window.dispatchEvent(event);
+};
 socket.onerror = (error) => console.log("websocket error", error);
 socket.onclose = (event) => console.log(event);
