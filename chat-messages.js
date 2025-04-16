f0b823a9e02359dc2d70c4a3cade3e591c259182
Benjamin Cofrancesco <benjamin@whitefoxboutique.com>
2025-04-16 15:17:06 +1000

    fix: changed to global

--- chat-messages.js
+++ chat-messages.js
@@ -7,51 +7,54 @@ class ChatMessages extends HTMLUListElement {
 
   async fetchEmotes() {
     try {
-      const response = await fetch('https://7tv.io/v3/emote-sets/popular');
+      const response = await fetch("https://7tv.io/v3/emote-sets/global");
       const data = await response.json();
 
       console.log(data);
-      
+
       const emotes = {};
-      
+
       if (data?.emotes) {
-        data.emotes.forEach(emote => {
+        data.emotes.forEach((emote) => {
           if (!emote?.data?.host) return;
-          
+
           const urls = [];
           const files = emote.data.host.files;
-          
-          files.forEach(file => {
+
+          files.forEach((file) => {
             if (file.format === "WEBP") {
-              const size = file.name.split('.')[0]; // e.g., "1x", "2x", etc.
+              const size = file.name.split(".")[0]; // e.g., "1x", "2x", etc.
               urls.push({
                 size,
-                url: `https:${emote.data.host.url}/${file.name}`
+                url: `https:${emote.data.host.url}/${file.name}`,
               });
             }
           });
-          
+
           urls.sort((a, b) => parseInt(a.size) - parseInt(b.size));
-          
+
           if (urls.length > 0) {
-            const chatUrl = urls.find(u => u.size === "1x") || urls[0];
-            emotes[emote.name] = `<img class="emote${emote.data.animated ? ' animated' : ''}" 
+            const chatUrl = urls.find((u) => u.size === "1x") || urls[0];
+            emotes[emote.name] =
+              `<img class="emote${emote.data.animated ? " animated" : ""}" 
               src="${chatUrl.url}" 
               alt="${emote.name}"
-              ${emote.data.flags?.zeroWidth ? 'style="margin: 0; width: 0;"' : ''}
+              ${emote.data.flags?.zeroWidth ? 'style="margin: 0; width: 0;"' : ""}
             />`;
           }
         });
       }
-     
-      console.log('Loaded emotes:', Object.keys(emotes).length);
+
+      console.log("Loaded emotes:", Object.keys(emotes).length);
       this.emotes = emotes;
     } catch (error) {
-      console.error('Failed to fetch emotes:', error);
+      console.error("Failed to fetch emotes:", error);
       this.emotes = {
-        "OMEGALUL": '<img class="emote" src="https://cdn.7tv.app/emote/60ae7216483a7a718f2eea91/1x.webp" alt="OMEGALUL" />',
-        "Pog": '<img class="emote" src="https://cdn.7tv.app/emote/60ae36af259ac5a73e56a424/1x.webp" alt="Pog" />',
-        "PagMan": '<img class="emote" src="https://cdn.7tv.app/emote/60ae858b229664e8667aea25/1x.webp" alt="PagMan" />'
+        OMEGALUL:
+          '<img class="emote" src="https://cdn.7tv.app/emote/60ae7216483a7a718f2eea91/1x.webp" alt="OMEGALUL" />',
+        Pog: '<img class="emote" src="https://cdn.7tv.app/emote/60ae36af259ac5a73e56a424/1x.webp" alt="Pog" />',
+        PagMan:
+          '<img class="emote" src="https://cdn.7tv.app/emote/60ae858b229664e8667aea25/1x.webp" alt="PagMan" />',
       };
     }
   }
@@ -71,13 +74,13 @@ class ChatMessages extends HTMLUListElement {
   }
 
   messageSanitise(msg) {
-    return msg.replace(/[&<>"']/g, function(match) {
+    return msg.replace(/[&<>"']/g, function (match) {
       const escape = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#39;'
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': "&quot;",
+        "'": "&#39;",
       };
       return escape[match];
     });
@@ -108,7 +111,7 @@ class ChatMessages extends HTMLUListElement {
         words[i] = this.emotes[word];
       }
     }
-    updated = words.join('');
+    updated = words.join("");
 
     return updated;
   }
